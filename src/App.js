5776027353773bf100ad29d7d3cefffac6a10a6e
ofu997 <ofu997@gmail.com>
2019-06-18 23:53:57 -0700

    start editing App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +24,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
