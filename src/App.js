64198867743eb6c9e2671898bb34205cccf8f4cb
ofu997 <ofu997@gmail.com>
2019-06-19 00:51:40 -0700

    map array to jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render() {
+    const helloWorld = 'Welcome to the Road to learn React';
     return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{helloWorld}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,6 +40,9 @@ class App extends Component {
         >
           Learn React
         </a>
+        {list.map(function(item) {
+        return <div>{item.title}</div>;
+        })}
       </header>
     </div>
     );
