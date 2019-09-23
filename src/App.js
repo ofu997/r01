274b34fbb1d721a3e5921a8cd31043753a1b5aa1
ofu997 <ofu997@gmail.com>
2019-09-22 23:58:28 -0700

    add searchTerm to state, ES6 destructuring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ENETDOWN } from 'constants';
 
 const list = [
   {
@@ -23,6 +24,15 @@ const list = [
 
 const helloWorld = 'Welcome to my React site';
 
+// function isSearched(searchTerm) {
+//   return function (item) {
+//     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+//   }
+// }
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
 
   constructor(props){
@@ -30,6 +40,7 @@ class App extends Component {
 
     this.state = {
       list,
+      searchTerm: '',
     };
     // bind a class method to constructor
     this.onDismiss = this.onDismiss.bind(this);
@@ -47,11 +58,12 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
-  onSearchChange() {
-
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -75,7 +87,7 @@ class App extends Component {
               onchange = {this.onSearchChange}
             />
           </form>
-          {this.state.list.map(item => 
+          {list.filter(isSearched(searchTerm)).map(item => 
             <div key = { item.objectID } >
               <span>
                 <a href={item.url}>{item.title}</a>
