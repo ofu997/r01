635b537bfe590a782c7f6b246402f37d32dc967a
ofu997 <ofu997@gmail.com>
2019-09-27 21:41:33 -0700

    delete comments

--- src/App.js
+++ src/App.js
@@ -63,41 +63,6 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      // <div className="App">
-      //   <form>
-      //     <input 
-      //       type="text" 
-      //       value = { searchTerm }
-      //       onChange = {this.onSearchChange}
-      //     />
-      //   </form>
-      //   {list.filter(isSearched(searchTerm)).map(item => 
-      //     <div key = { item.objectID } >
-      //       <span>
-      //         <a href={item.url}>{item.title}</a>
-      //       </span>
-      //       <span>
-      //         {item.author}&nbsp;
-      //       </span>
-      //       <span>
-      //         comments: {item.num_comments}&nbsp;
-      //       </span>
-      //       <span>
-      //         points: {item.points}
-      //       </span>
-      //       <span>
-      //         <button
-      //         onClick={() => this.onDismiss(item.objectID)}
-      //         type="button"
-      //         >
-      //         Dismiss
-      //         </button>
-      //       </span>
-      //     </div>
-      //   )}
-      // </div> //for class App
-
-      // Split App into components
       <div className="App">
         <Search 
           value = {searchTerm}
