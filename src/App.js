b663f0ce3044f30379fdbcbb22e0ac3d010441ca
ofu997 <ofu997@gmail.com>
2020-02-19 17:53:23 -0800

    apply more css and make reusable components into functional stateless components

--- src/App.js
+++ src/App.js
@@ -54,18 +54,20 @@ class App extends Component {
     // var list = this.state.list;
   const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <Search 
-          value = { searchTerm }
-          onChange = { this.onSearchChange }
-        >
-          Search 
-        </Search>
-        <Table 
-          list = { list }
-          pattern = { searchTerm }
-          onDismiss = { this.onDismiss }
-        />
+      <div className='page'>
+        <div className='interactions'>
+          <Search 
+            value = { searchTerm }
+            onChange = { this.onSearchChange }
+          >
+            Search 
+          </Search>
+          <Table 
+            list = { list }
+            pattern = { searchTerm }
+            onDismiss = { this.onDismiss }
+          />
+        </div>
       </div>
     );
   }
@@ -128,53 +130,82 @@ const Search = ( { value, onChange, children } ) =>
       onChange={onChange}
     />
   </form>
-  
-  
 
+// class Table extends Component {
+//   render() {
+//     const { list, pattern, onDismiss } = this.props;
+//     return(
+//       <div>
+//         {list.filter(isSearched(pattern)).map(item =>
+//           <div key = { item.objectID }>
+//             <span><a href = { item.url }>{item.title}</a></span>
+//             <span>{ item.author }</span>
+//             <span>{ item.num_comments }</span>
+//             <span>{ item.points }</span>
+//             <span>
+//               <Button
+//                 onClick={() => onDismiss(item.objectID)}
+//               >
+//                 Dismiss
+//               </Button>
+//             </span>
+//           </div>
+//         )}
+//       </div>
+//     )
+//   }
+// }
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return(
-      <div>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key = { item.objectID }>
-            <span><a href = { item.url }>{item.title}</a></span>
-            <span>{ item.author }</span>
-            <span>{ item.num_comments }</span>
-            <span>{ item.points }</span>
-            <span>
-              <Button
-                onClick={() => onDismiss(item.objectID)}
-              >
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        )}
+// As a functional stateless component
+const Table = ({ list, pattern, onDismiss }) =>  
+  <div>
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key = { item.objectID }>
+        <span><a href = { item.url }>{item.title}</a></span>
+        <span>{ item.author }</span>
+        <span>{ item.num_comments }</span>
+        <span>{ item.points }</span>
+        <span>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+          >
+            Dismiss
+          </Button>
+        </span>
       </div>
-    )
-  }
-}
+    )}
+  </div>
+
+
 
 // example of reusable component
-class Button extends Component {
-  render() {
-    const { 
-      onClick, 
-      className = "", 
-      children,
-    } = this.props;
-    return (
-      <button
-      onClick = { onClick }
-      className = { className }
-      type="button"
-      >
-      { children }
-      </button>
-    );
-  }
-}
+// class Button extends Component {
+//   render() {
+//     const { 
+//       onClick, 
+//       className = "", 
+//       children,
+//     } = this.props;
+//     return (
+//       <button
+//       onClick = { onClick }
+//       className = { className }
+//       type="button"
+//       >
+//       { children }
+//       </button>
+//     );
+//   }
+// }
+
+// As a functional stateless component
+const Button = ({ onClick, className, children}) => 
+  <button
+  onClick = { onClick }
+  className = { className }
+  type="button"
+  >
+  { children }
+  </button>
 
 export default App;
