0812288466857f7e423dcd1e629a64ba013a9a82
ofu997 <ofu997@gmail.com>
2020-02-23 15:21:15 -0800

    prepare for third party api

--- src/App.js
+++ src/App.js
@@ -3,39 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 import { ENETDOWN } from 'constants';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+// const list = [
+//   {
+//     title: 'React',
+//     url: 'https://reactjs.org/',
+//     author: 'Jordan Walke',
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: 'Redux',
+//     url: 'https://redux.js.org/',
+//     author: 'Dan Abramov, Andrew Clark',
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   },
+// ];
 
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 // uses local state so is an ES6 class component. The rest are Functional Stateless Components
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      list,
-      searchTerm: '',
+      // list,
+      // searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
     // bind a class method to constructor
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+  
+  // You use the componentDidMount() lifecycle method to fetch the data after the component
+  // mounted. The first fetch uses default search term from the local state. It will fetch “redux” related
+  // stories, because that is the default parameter.
+  componentDidMount() {
+    const { searchTerm } = this.state; 
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   // arrow function would auto bind the function
