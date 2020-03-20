ba16539dd157e50794159a4ca04fdc622bb72b0e
ofu997 <ofu997@gmail.com>
2020-03-19 23:31:48 -0700

    add tests, move constants to file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const DEFAULT_QUERY = 'redux';
-const DEFAULT_HPP = '100';
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
+import { DEFAULT_QUERY, DEFAULT_HPP, PATH_BASE,
+  PATH_SEARCH, PARAM_SEARCH, PARAM_PAGE,
+  PARAM_HPP } from './constants';
 
 // uses local state so is an ES6 class component. The rest are Functional Stateless Components
 class App extends Component {
@@ -214,3 +210,9 @@ const Button = ({ onClick, className = '', children}) =>
   </button>
 
 export default App;
+
+export {
+  Button,
+  Search,
+  Table,
+};
